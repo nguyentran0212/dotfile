79dce092b6bc89e3adfe24f91b934f5a427aef7c
nguyentran0212 <gen.tran.1991@gmail.com>
2025-05-02 11:35:28 +0930

    Updated aichat config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -28,7 +28,7 @@ return {
     "lervag/vimtex",
     lazy = false,
     init = function()
-      vim.g.vimtex_view_method = "zathura"
+      -- vim.g.vimtex_view_general_viewer = "okular"
       vim.g.vimtex_fold_enabled = 1
       vim.g.vimtex_complete_enabled = 0
     end
